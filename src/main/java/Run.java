c464375f651bac0cb3c6c0f06041f7c93d86bcf8
noriamas1995 <43602821+noriamas1995@users.noreply.github.com>
2021-06-17 21:21:12 +0800

    Initial commit

---
+++ src/main/java/Run.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Run {
+    public static Map<Integer, String> methodMap;
+
+    public static void main(String[] args) {
+
+    }
+}
